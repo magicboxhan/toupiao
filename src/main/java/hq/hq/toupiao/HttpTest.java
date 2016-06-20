2c4ee29eaada2576906f672fec81dad4269f9bc8
hq11258 <hq11258@ly.com>
2016-06-20 12:39:14 +0800

    测试手机号修正

--- src/main/java/hq/hq/toupiao/HttpTest.java
+++ src/main/java/hq/hq/toupiao/HttpTest.java
@@ -58,27 +58,22 @@ public class HttpTest {
     public void memberTest() throws IOException{
         CloseableHttpClient httpclient = HttpClients.createDefault();
         try {
+            //请求地址
             HttpPost httpPost = new HttpPost("http://10.14.41.165:8011/Service.ashx");
-            String data = "201204261002{\"AccountID\":\"f97f1493-692e-483d-bea0-1937f269c42a\",\"MethodName\":\"QueryMember\",\"MethodVersion\":1,\"Namespace\":\"TongCheng.SOA.Interface.TCMember.Main.Entities\",\"ReqTime\":\"\\/Date(1465366892273+0800)\\/\",\"Version\":\"201204261002\"}#tcsoa#TongCheng.SOA.Interface.TCMember.Main.Entities.QueryMemberRequest#tcsoa#TongCheng.SOA.Interface.TCMember.Main.Entities, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null#tcsoa#{\"AccessToken\":null,\"Act\":{\"AppKey\":\"QA.Test\",\"AppSecret\":\"2e44d22f-5353-40cc-a3c1-0819faf0fb86\"},\"Ip\":\"10.101.64.72\",\"MemberSystem\":0,\"QueryKey\":13000000000,\"QueryType\":3}";
+            //Post字符串
+            String data = "201204261002{\"AccountID\":\"f97f1493-692e-483d-bea0-1937f269c42a\",\"MethodName\":\"QueryMember\",\"MethodVersion\":1,\"Namespace\":\"TongCheng.SOA.Interface.TCMember.Main.Entities\",\"ReqTime\":\"\\/Date(1465366892273+0800)\\/\",\"Version\":\"201204261002\"}#tcsoa#TongCheng.SOA.Interface.TCMember.Main.Entities.QueryMemberRequest#tcsoa#TongCheng.SOA.Interface.TCMember.Main.Entities, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null#tcsoa#{\"AccessToken\":null,\"Act\":{\"AppKey\":\"QA.Test\",\"AppSecret\":\"2e44d22f-5353-40cc-a3c1-0819faf0fb86\"},\"Ip\":\"10.101.64.72\",\"MemberSystem\":0,\"QueryKey\":18652332951,\"QueryType\":3}";
 
-//            httpPost.setHeader("User-Agent", "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/7.0)");
-//            httpPost.setHeader("Accept", "application/x-ms-application, image/jpeg, application/xaml+xml, image/gif, image/pjpeg, application/x-ms-xbap, application/vnd.ms-excel, application/vnd.ms-powerpoint, application/msword, */*");
-//            httpPost.setHeader("Accept-Language", "zh-CN");
             httpPost.setEntity(new StringEntity(data));
-            CloseableHttpResponse response2 = httpclient.execute(httpPost);
-
-
+            CloseableHttpResponse response = httpclient.execute(httpPost);
 
             try {
-//            	System.out.println("测试中文");
-//                System.out.println(response2.getStatusLine());
-                HttpEntity entity2 = response2.getEntity();
-                System.out.println(EntityUtils.toString(entity2));
+                HttpEntity entity = response.getEntity();
+                System.out.println(EntityUtils.toString(entity));
                 // do something useful with the response body
                 // and ensure it is fully consumed
-                EntityUtils.consume(entity2);
+                EntityUtils.consume(entity);
             } finally {
-                response2.close();
+                response.close();
             }
         } finally {
             httpclient.close();
