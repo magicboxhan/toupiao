f914978d8fb644e22603c88ba6041d7170ede96b
hq11258 <hq11258@ly.com>
2016-06-20 12:28:13 +0800

    会员接口调用实例

--- src/main/java/hq/hq/toupiao/App.java
+++ src/main/java/hq/hq/toupiao/App.java
@@ -1,7 +1,6 @@
 package hq.hq.toupiao;
 
-import java.util.Date;
-import java.util.Random;
+import java.io.IOException;
 
 /**
  * Hello world!
@@ -12,27 +11,32 @@ public class App
     public static void main( String[] args )
     {
 		HttpTest h = new HttpTest();
-		int ip1 = 0;
-		int ip2 = 0;
-		String ipString = "";
-		Random r=new java.util.Random();
-		while(true){
-			int randomIndex_ip1 = Math.abs(r.nextInt()) % 119;
-			int randomIndex_ip2 = Math.abs(r.nextInt()) % 255;
-			ip1 = randomIndex_ip1 + 137;
-			ip2 = randomIndex_ip2 + 1;
-			Date date = new Date();
-			ipString = String.format("61.155.%d.%d", ip1, ip2);
-			try{
-				System.out.println();
-				System.out.println();
-				System.out.println(String.format("%s: Using IP: %s", date.toString(), ipString));
-				h.toupiao(ipString);
-				Thread.sleep(20000);
-			}catch(Exception e){
-				e.printStackTrace();
-				continue;
-			}
+		try {
+			h.memberTest();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+//		int ip1 = 0;
+//		int ip2 = 0;
+//		String ipString = "";
+//		Random r=new java.util.Random();
+//		while(true){
+//			int randomIndex_ip1 = Math.abs(r.nextInt()) % 119;
+//			int randomIndex_ip2 = Math.abs(r.nextInt()) % 255;
+//			ip1 = randomIndex_ip1 + 137;
+//			ip2 = randomIndex_ip2 + 1;
+//			Date date = new Date();
+//			ipString = String.format("61.155.%d.%d", ip1, ip2);
+//			try{
+//				System.out.println();
+//				System.out.println();
+//				System.out.println(String.format("%s: Using IP: %s", date.toString(), ipString));
+//				h.toupiao(ipString);
+//				Thread.sleep(20000);
+//			}catch(Exception e){
+//				e.printStackTrace();
+//				continue;
+//			}
 			
 //			for(ip1=137;ip1<=255;ip1++){
 //				for(ip2=1;ip2<=255;ip2++){
@@ -49,4 +53,4 @@ public class App
 //			}
 		}
     }
-}
+//}
--- src/main/java/hq/hq/toupiao/HttpTest.java
+++ src/main/java/hq/hq/toupiao/HttpTest.java
@@ -1,22 +1,19 @@
 package hq.hq.toupiao;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
 import org.apache.http.HttpEntity;
-import org.apache.http.HttpHost;
-import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
-import org.apache.http.impl.conn.DefaultProxyRoutePlanner;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class HttpTest {
 
@@ -57,6 +54,36 @@ public class HttpTest {
             httpclient.close();
         }
 	}
+
+    public void memberTest() throws IOException{
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        try {
+            HttpPost httpPost = new HttpPost("http://10.14.41.165:8011/Service.ashx");
+            String data = "201204261002{\"AccountID\":\"f97f1493-692e-483d-bea0-1937f269c42a\",\"MethodName\":\"QueryMember\",\"MethodVersion\":1,\"Namespace\":\"TongCheng.SOA.Interface.TCMember.Main.Entities\",\"ReqTime\":\"\\/Date(1465366892273+0800)\\/\",\"Version\":\"201204261002\"}#tcsoa#TongCheng.SOA.Interface.TCMember.Main.Entities.QueryMemberRequest#tcsoa#TongCheng.SOA.Interface.TCMember.Main.Entities, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null#tcsoa#{\"AccessToken\":null,\"Act\":{\"AppKey\":\"QA.Test\",\"AppSecret\":\"2e44d22f-5353-40cc-a3c1-0819faf0fb86\"},\"Ip\":\"10.101.64.72\",\"MemberSystem\":0,\"QueryKey\":13000000000,\"QueryType\":3}";
+
+//            httpPost.setHeader("User-Agent", "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/7.0)");
+//            httpPost.setHeader("Accept", "application/x-ms-application, image/jpeg, application/xaml+xml, image/gif, image/pjpeg, application/x-ms-xbap, application/vnd.ms-excel, application/vnd.ms-powerpoint, application/msword, */*");
+//            httpPost.setHeader("Accept-Language", "zh-CN");
+            httpPost.setEntity(new StringEntity(data));
+            CloseableHttpResponse response2 = httpclient.execute(httpPost);
+
+
+
+            try {
+//            	System.out.println("测试中文");
+//                System.out.println(response2.getStatusLine());
+                HttpEntity entity2 = response2.getEntity();
+                System.out.println(EntityUtils.toString(entity2));
+                // do something useful with the response body
+                // and ensure it is fully consumed
+                EntityUtils.consume(entity2);
+            } finally {
+                response2.close();
+            }
+        } finally {
+            httpclient.close();
+        }
+    }
 	
 
 }
